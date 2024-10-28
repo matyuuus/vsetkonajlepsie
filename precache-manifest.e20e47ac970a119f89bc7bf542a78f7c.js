ee0256230408145b50f59a5e6d6c0365e287e150
Matus <matus@Matuss-MBP.home>
2024-10-28 14:57:40 +0100

    Updates

---
+++ precache-manifest.e20e47ac970a119f89bc7bf542a78f7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ece25fc2d6dad72cc4c2af57b01e65ff",
+    "url": "/matyuuus/vsetkonajlepsie.git/index.html"
+  },
+  {
+    "revision": "70f97c6d64bf8d75c872",
+    "url": "/matyuuus/vsetkonajlepsie.git/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/matyuuus/vsetkonajlepsie.git/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "e2a960147ef5362ced8a",
+    "url": "/matyuuus/vsetkonajlepsie.git/static/js/3.1b3c587a.chunk.js"
+  },
+  {
+    "revision": "70f97c6d64bf8d75c872",
+    "url": "/matyuuus/vsetkonajlepsie.git/static/js/main.cba4cd03.chunk.js"
+  },
+  {
+    "revision": "75866f177c2729effb95",
+    "url": "/matyuuus/vsetkonajlepsie.git/static/js/runtime~main.ab9dc121.js"
+  }
+]);
